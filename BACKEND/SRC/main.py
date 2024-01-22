a427e7b76d6f39c242dd083471ecdf6d3e4a5a38
YashwanthSimhaa <yashwanth.ravish@estuate.com>
2024-01-22 16:54:58 +0530

    Added demo function

--- BACKEND/SRC/main.py
+++ BACKEND/SRC/main.py
@@ -214,3 +214,6 @@ async def delete_by_id(item_id : str):
     except Exception :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="File Not Found")   
     
+@app.get("/demo")
+async def demo():
+    pass
